de7bfa18be55e8c273031e99f5eb458faae8d158
Danish Hacker <83761623+danishhacker420@users.noreply.github.com>
2024-03-04 11:52:34 +0530

    Delete Danish.py

--- Danish.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-Danish=platform.architecture()[0]
-if Danish=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif Danish=="64bit":
-     __import__("Danish")
